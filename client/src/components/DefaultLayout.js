6e3e739dafbd4aa44a9741a154ca6778b8acfaed
Udaykumar Sanjay Gadikar <74957465+Udayraj2806@users.noreply.github.com>
2022-07-28 10:18:00 +0530

    Update DefaultLayout.js

--- client/src/components/DefaultLayout.js
+++ client/src/components/DefaultLayout.js
@@ -36,7 +36,7 @@ const DefaultLayout = (props) => {
       )}
       <Sider trigger={null} collapsible collapsed={collapsed}>
         <div className="logo">
-          <h3>{collapsed ? "SP" : "SHEY POS"}</h3>
+          <h3>{collapsed ? "FP" : "FLEX POS"}</h3>
         </div>
         <Menu
           theme="dark"
